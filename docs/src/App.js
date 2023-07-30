b7c756a58398e96476f30115fd3347fe5faccede
Sadman Sakib <ssakib4040@gmail.com>
2023-07-31 00:31:46 +0600

    Button Component added

--- docs/src/App.js
+++ docs/src/App.js
@@ -5,7 +5,26 @@ import { Button } from "reactomnix/dist/reactomnix";
 function App() {
   return (
     <div className="App">
-      <Button>This is a button</Button>
+      <Button onClick={() => console.log("Button Clicked!")}>Click Me</Button>
+      <Button preText="🚀" onClick={() => console.log("Launch Clicked!")}>
+        Launch
+      </Button>
+
+      <Button postText="👋" onClick={() => console.log("Say Hi Clicked!")}>
+        Say Hi
+      </Button>
+
+      <Button
+        className="custom-button"
+        text="s"
+        onClick={() => console.log("Custom Class Clicked!")}
+      >
+        Custom Class
+      </Button>
+
+      <Button onClick={() => console.log("Custom Children Clicked!")}>
+        <span style={{ color: "blue" }}>I'm a Custom Child</span>
+      </Button>
     </div>
   );
 }
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,31 @@
 import React from "react";
+import classNames from "classnames";
 import "./Button.scss";
 
 interface ButtonProps {
-  children: React.ReactNode;
+  preText?: string;
+  postText?: string;
+  className?: string;
+  children?: React.ReactNode;
   onClick: () => void;
 }
 
-const Button: React.FC<ButtonProps> = ({ children, onClick }) => {
-  console.log(`Yee`);
-  return <button onClick={onClick}>{children}</button>;
+const Button: React.FC<ButtonProps> = ({
+  preText,
+  postText,
+  className,
+  onClick,
+  children,
+}) => {
+  const buttonClassName = classNames("button", className);
+
+  return (
+    <button className={buttonClassName} onClick={onClick}>
+      {preText && <span className="pre-text">{preText}</span>}
+      {children}
+      {postText && <span className="post-text">{postText}</span>}
+    </button>
+  );
 };
 
 export default Button;
